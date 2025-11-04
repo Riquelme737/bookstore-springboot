4d3853e53d391784eb3675f8c63394c4ff1f58c2
Riquelme Oliveira Santos <7riquelmesantos2006@gmail.com>
2025-11-04 20:27:00 -0300

    @JsonProperty and (fetch)

--- src/main/java/com/bookstore/jpa/models/BookModel.java
+++ src/main/java/com/bookstore/jpa/models/BookModel.java
@@ -1,5 +1,6 @@
 package com.bookstore.jpa.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -25,7 +26,8 @@ public class BookModel implements Serializable {
     @JoinColumn(name = "publisher_id")
     private PublisherModel publisher;
 
-    @ManyToMany
+    //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany//(fetch = FetchType.LAZY)
     @JoinTable(
             name = "tb_book_author",
             joinColumns = @JoinColumn(name = "book_id"),
